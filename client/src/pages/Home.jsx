041dca5c6c379626ef23edaf554a83fcbbd8a943
Nithin S J <nithinjayanjasla@gmail.com>
2025-03-08 12:18:49 +0530

    client api update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('/api/v1/post', {
+        const response = await fetch('https://ai-image-gen-0cxn.onrender.com/api/v1/post', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
